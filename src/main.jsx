8f07c68573e3c2b42f7a61ce527b8316e9b1dd06
Muhammad Ismail <60378436+ismail855@users.noreply.github.com>
2023-09-08 18:17:03 +0600

    added

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
+import ReactDOM from 'react-dom/client';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </React.StrictMode>,
+)
